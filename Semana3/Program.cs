f080aa42d1ffd74d08e2076d9b95f8490ffa21bc
Sebas <sebas@gmail.com>
2025-04-10 12:09:14 -0500

    ejercicio1

---
+++ Semana3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string nombre;
+            Console.WriteLine("Ingresa tu nombre");
+            nombre = Console.ReadLine();
+
+            Console.WriteLine("Bienvenidos al mundo de la programación" + nombre);
+
+
+        }
+
+        
+    }    }
